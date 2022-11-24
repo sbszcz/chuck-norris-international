2562b778e013a34734b2e986ee902e12c06bcf39
Sebastian Barszczewski <sebastian.barszczewski@gmail.com>
2022-11-24 14:32:22 +0100

    cleanup ...
    - introduced gradle version catalog
    - using java 19
    - bumped spring-boot to 3.0.0-RC2
    - fixed gradle test tasks configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
-
+//todo remove this suppression when https://github.com/gradle/gradle/issues/22797 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("org.springframework.boot") version "2.7.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("java")
-
-    id("org.unbroken-dome.test-sets") version "4.0.0"
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.testsets)
 }
 
 group = "dev.sbszcz"
@@ -17,12 +12,14 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
 
+    // TODO this is just temporary as Spring Boot 3 is still a milestone. No release yet
+    maven { url = uri("https://repo.spring.io/milestone")}
+}
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(18))
+        languageVersion.set(JavaLanguageVersion.of(19))
     }
 }
 
@@ -33,42 +30,30 @@ testSets {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(libs.spring.boot.starter)
+    implementation(libs.spring.boot.starter.web)
+    developmentOnly(libs.spring.boot.devtools)
 
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
-    implementation("org.jetbrains:annotations:20.1.0")
+    implementation(libs.apache.httpclient)
+    implementation(libs.jetbrains.annotations)
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.1")
-    testImplementation("org.assertj:assertj-core:3.23.1")
-    testImplementation("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.wiremock)
+    testImplementation(libs.assertj)
+    testImplementation(libs.jsonassert)
+    testImplementation(libs.javax.annotation.api)
 }
 
-val test by tasks.getting(Test::class) {
+tasks.withType<Test> {
     useJUnitPlatform()
-
     testLogging {
         events = setOf(
-            PASSED,
-            SKIPPED,
-            FAILED,
-            STANDARD_ERROR,
-            STANDARD_OUT
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+//            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+//            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
         )
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     }
-
-    // always run this task
-    outputs.upToDateWhen { false }
-}
-
-val componentTest by tasks.named<Test>("componentTest") {
-    useJUnitPlatform()
-
-    systemProperties(System.getProperties().mapKeys { it.key as String })
-
-    // always run this task
-    outputs.upToDateWhen { false }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "chuck-norris-international"
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        // TODO this is just temporary as Spring Boot 3 is still a milestone. No release yet
+        maven { url = uri("https://repo.spring.io/milestone") }
+        gradlePluginPortal()
+    }
+}
